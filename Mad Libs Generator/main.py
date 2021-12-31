b07916fe0116a09e4d823d202de2dcd18f66710a
darkraven92 <ekludvig92@gmail.com>
2021-12-31 02:31:07 +0100

    Commit Done

---
+++ Mad Libs Generator/main.py
@@ -0,0 +1,20 @@
+def madlibs():
+
+    while True:
+        print("Welcome to this Madlibs Generator!")
+        print("Fill out these questions to generate your own silly story")
+        print("----------------------------------------------------------\n")
+        adj = input("enter a adjective: ")
+        foods1 = input("enter foods: ")
+        verb = input("enter a verb: ")
+        saying = input("enter a saying: ")
+        noun = input("enter a noun: ")
+        foods2 = input("enter foods: ")
+        color = input("enter a color: ")
+        ride = input("enter something you would ride in: ")
+        animal = input("enter a animal: ")
+        person = input("enter a person: ")
+
+        print("", adj, "",foods1,"",foods2,"",verb,"",saying,"",noun,"",color,"",ride,"",animal,"",person)
+
+madlibs()
